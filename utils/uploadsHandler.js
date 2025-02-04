e25ed57c5b3d9543296f8e9ebc545d32bff4aac0
msi-end <aditya01377@gmail.com>
2025-02-04 22:05:23 +0530

    new updates

--- utils/uploadsHandler.js
+++ utils/uploadsHandler.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
     storage,
-    limits: { fileSize: 10 * 1024 * 1024 }, // Limit files to 10MB
+    limits: { fileSize: 20 * 1024 * 1024 }, // Limit files to 10MB
 });
 
 module.exports = upload;
